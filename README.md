768a030bbd0d9d074819acebb56d2a16aa05aa19
YanisBft <yanis.briffaut@gmail.com>
2025-01-20 21:38:36 +0100

    Init repository

---
+++ README.md
@@ -0,0 +1,14 @@
+# CommandHookPaper
+Entity selector parsing for non-vanilla commands.
+
+Only compatible with Paper servers from **1.21.3** to **1.21.4**.
+
+This plugin acts as a simple replacement for [CommandHook](https://github.com/NewAgeCZ/CommandHook),
+which does not work anymore in 1.21.3+. Therefore, the vast majority of the code was made by its author.
+
+----
+
+## License
+[![GPL-3.0 License](https://img.shields.io/github/license/NewAgeCZ/CommandHook?&logo=github)](LICENSE)
+
+All files are licensed under GPL 3.0 license
---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+  `java-library`
+  id("io.papermc.paperweight.userdev") version "2.0.0-beta.14"
+  id("xyz.jpenilla.run-paper") version "2.3.1" // Adds runServer and runMojangMappedServer tasks for testing
+  id("xyz.jpenilla.resource-factory-paper-convention") version "1.2.0" // Generates plugin.yml based on the Gradle config
+}
+
+group = "com.yanisbft.commandhookpaper"
+description = property("plugin.description").toString()
+version = property("plugin.version").toString()
+
+java {
+  // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 11 installed for example.
+  toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+dependencies {
+  paperweight.paperDevBundle("1.21.3-R0.1-SNAPSHOT")
+
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+  testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+  testImplementation("org.mockito:mockito-core:5.11.0")
+}
+
+tasks {
+  compileJava {
+    options.release = 21
+  }
+  javadoc {
+    options.encoding = Charsets.UTF_8.name()
+  }
+  test {
+    useJUnitPlatform()
+  }
+}
+
+paperPluginYaml {
+  main = property("paper.main-class").toString()
+  apiVersion = property("paper.api-version").toString()
+  authors.add("YanisBft")
+  authors.add("_NewAge")
+}
