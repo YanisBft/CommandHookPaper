bfa86db89395d64c6c924bdbb95669f2ccd8e83c
YanisBft <yanis.briffaut@gmail.com>
2026-08-02 03:33:01 +0200

    Update gradle, java & junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  `java-library`
-  id("io.papermc.paperweight.userdev") version "2.0.0-beta.18"
+  id("java")
+  id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
   id("xyz.jpenilla.run-paper") version "2.3.1" // Adds runServer and runMojangMappedServer tasks for testing
   id("xyz.jpenilla.resource-factory-paper-convention") version "1.2.0" // Generates plugin.yml based on the Gradle config
 }
@@ -10,21 +10,20 @@ description = property("plugin.description").toString()
 version = property("plugin.version").toString()
 
 java {
-  // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 11 installed for example.
-  toolchain.languageVersion = JavaLanguageVersion.of(21)
+  toolchain.languageVersion = JavaLanguageVersion.of(25)
 }
 
 dependencies {
   paperweight.paperDevBundle("1.21.11-R0.1-SNAPSHOT")
 
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
-  testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.1.2")
+  testImplementation("org.junit.jupiter:junit-jupiter:6.1.2")
   testImplementation("org.mockito:mockito-core:5.20.0")
 }
 
 tasks {
   compileJava {
-    options.release = 21
+    options.release = 25
   }
   javadoc {
     options.encoding = Charsets.UTF_8.name()
